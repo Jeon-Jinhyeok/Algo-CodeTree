d8ad284eb1c188f4541cba568ae491b498181b86
Jinhyeok <aqwstn@gmail.com>
2026-04-17 16:45:11 +0900

    Title: 계단 오르기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260417/계단 오르기/climbing-stairs.java
+++ 260417/계단 오르기/climbing-stairs.java
@@ -4,9 +4,7 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         int n = sc.nextInt();
-
         int[] dp = new int[n+1];
 
         dp[2] = 1;
@@ -15,10 +13,10 @@ public class Main {
             dp[3] = 1;
 
         for(int i = 4; i <= n; i++){
-            dp[i] = dp[i-2] + dp[i-3];
+            dp[i] = (dp[i-2] + dp[i-3]) % 10007;
         }
 
-        System.out.println(dp[n]);
+        System.out.println(dp[n] % 10007);
         sc.close();
     }
 }
