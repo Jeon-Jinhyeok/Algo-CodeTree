bde0ee27f8b6a6d6857a85ef0b11e871cdcd91a1
Jinhyeok <aqwstn@gmail.com>
2026-04-20 16:28:27 +0900

    Title: 정수 사각형 최대 합, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260420/정수 사각형 최대 합/maximum-sum-path-in-square.java
+++ 260420/정수 사각형 최대 합/maximum-sum-path-in-square.java
@@ -1,4 +1,3 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,26 +7,32 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		int[][] map = new int[N+1][N+1];
+		int[][] map = new int[N][N];
 		
 		
-		for(int i = 1; i <= N; i++) {
+		for(int i = 0; i < N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			
-			for(int j = 1; j <= N; j++) {
+			for(int j = 0; j < N; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
 		
-		int[][] dp = new int[N+1][N+1];
-		for(int i = 1; i <= N; i++) {
-			for(int j = 1; j <= N; j++) {
-				dp[i][j] = Math.max(dp[i-1][j] + map[i][j], dp[i][j-1] + map[i][j]);
-				System.out.println();
-			}
+		int[][] dp = new int[N][N];
+		dp[0][0] = map[0][0];
+		
+		for(int i = 1; i < N; i++) {
+			dp[i][0] = dp[i-1][0] + map[i][0];
+			dp[0][i] = dp[0][i-1] + map[0][i];
 		}
 		
-		System.out.println(dp[N][N]);
+		for(int i = 1; i < N; i++) {
+			for(int j = 1; j < N; j++) {
+				dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]) + map[i][j];
+			}
+		}
+
+		System.out.println(dp[N-1][N-1]);
 	}
 
 }
